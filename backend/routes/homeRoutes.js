7992b55e0eb58145caf01dada00fcc6e3e35e867
anxnt-goyl <anantg0609@gmail.com>
2026-04-27 15:19:27 +0530

    home page

--- backend/routes/homeRoutes.js
+++ backend/routes/homeRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const Patient = require("../models/Patient");
 
+router.get("/", async (req, res) => {
+  const hospitals = await Patient.distinct("hospital");
 
-router.get('/', (req, res) => {
-    res.render('home_dashboard/index');
+  res.render("home_dashboard/index", { hospitals });
 });
+
+
+
 
 module.exports = router;
